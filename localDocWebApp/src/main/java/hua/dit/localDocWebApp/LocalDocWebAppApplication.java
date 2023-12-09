68f0e2b23b2c5ddce30cbf0946b8aa9a615e5287
IOANNIS ROUMELIOTIS <it2021087@hua.gr>
2023-12-09 15:54:36 +0200

    prwto

---
+++ localDocWebApp/src/main/java/hua/dit/localDocWebApp/LocalDocWebAppApplication.java
@@ -0,0 +1,13 @@
+package hua.dit.localDocWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalDocWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalDocWebAppApplication.class, args);
+	}
+
+}
---
+++ localDocWebApp/src/test/java/hua/dit/localDocWebApp/LocalDocWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hua.dit.localDocWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalDocWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
