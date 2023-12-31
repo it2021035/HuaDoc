c372fff13d5dfeec1c41b7a63bc0e025f4efc1e7
it2021035 <118733102+it2021035@users.noreply.github.com>
2023-12-31 22:24:13 +0200

    Adden a login form ,sing in sign out button and put different action on different roles(No roles are created yet ;D)

---
+++ localDocWebApp/src/main/java/hua/dit/localDocWebApp/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package hua.dit.localDocWebApp.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home","/register","/saveUser").permitAll()
+                        .requestMatchers("/client/new","/doctor/list").hasRole("CLIENT")
+                        .requestMatchers("/doctor/new","/client/list","/pending/show").hasRole("DOCTOR")
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+
+    }
+
+}
