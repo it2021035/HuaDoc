80bad85d7247ec9a3f20a6dc04eaad3a5cdb1e21
it2021035 <118733102+it2021035@users.noreply.github.com>
2024-01-04 23:42:46 +0200

    Added login form Roles Users User Services Role,user repository sec and app config

---
+++ localDocWebApp/src/main/java/hua/dit/localDocWebApp/entity/ERole.java
@@ -0,0 +1,8 @@
+package hua.dit.localDocWebApp.entity;
+
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ localDocWebApp/src/main/java/hua/dit/localDocWebApp/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package hua.dit.localDocWebApp.repository;
+
+import hua.dit.localDocWebApp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+}
